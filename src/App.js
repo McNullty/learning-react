abd830e9a25c106eefe596f94be13c8bb92d50d5
Mladen Čikara <mladen.cikara@gmail.com>
2020-05-27 09:52:14 +0200

    Using map to display list of elements

--- src/App.js
+++ src/App.js
@@ -6,10 +6,29 @@ const title = 'React'
 const welcome = {
     greeting: 'Hey',
     title: 'React',
-}
+};
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 
 function App() {
-  return helloWorld2();
+  return helloWorld3();
 }
 
 export default App;
@@ -39,6 +58,29 @@ export function helloWorld2() {
 
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
+        </div>
+    );
+}
+
+export function helloWorld3() {
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
+
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+
+            <hr />
+
+            {list.map(item =>
+                <div key={item.objectID}>
+                    <span>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                </div>)}
         </div>
     );
 }

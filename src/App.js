d3c79f6726dbb729432e20a75d5262c41998d6ea
Mladen Čikara <mladen.cikara@gmail.com>
2020-05-27 10:06:09 +0200

    Extracting React component

--- src/App.js
+++ src/App.js
@@ -72,15 +72,22 @@ export function helloWorld3() {
 
             <hr />
 
-            {list.map(item =>
-                <div key={item.objectID}>
-                    <span>
-                        <a href={item.url}>{item.title}</a>
-                    </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                </div>)}
+            <List />
         </div>
     );
+}
+
+function List() {
+    return list.map(function(item) {
+        return (
+            <div key={item.objectID}>
+                <span>
+                <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </div>
+        );
+    });
 }

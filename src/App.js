d3e19a85e67bfb126860a1f66f13c16fa805eac2
Mladen Čikara <mladen.cikara@gmail.com>
2020-05-28 09:35:56 +0200

    Refactor: Making components more concise.

--- src/App.js
+++ src/App.js
@@ -48,16 +48,25 @@ const App = () => {
         },
     ];
 
+    const [searchTerm, setSearchTerm] = React.useState('React');
+
+    const handleSearch = event => {
+        setSearchTerm(event.target.value);
+    };
+
+    const searchStories = stories.filter(story => story.title
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase()));
 
     return (
         <div>
             <h1>My Hacker Stories</h1>
 
-            <Search/>
+            <Search search={searchTerm} onSearch={handleSearch}/>
 
             <hr/>
 
-            <List list={stories}/>
+            <List list={searchStories}/>
         </div>
     );
 
@@ -132,14 +141,16 @@ export const helloWorld3 = () => {
     );
 }
 
-const List = props =>
-    props.list.map(item => (
-        <div key={item.objectID}>
-                <span>
-                <a href={item.url}>{item.title}</a>
-                </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-        </div>
-    ));
+const List = ({list}) =>
+    list.map(({objectID, ...item}) => <Item key={objectID} {...item}/>);
+
+const Item = ({title, url, author, num_comments, points}) => (
+    <div>
+        <span>
+            <a href={url}>{title}</a>
+        </span>
+        <span>{author}</span>
+        <span>{num_comments}</span>
+        <span>{points}</span>
+    </div>
+);
--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,13 @@
 import React from "react";
 
-export const Search = () => {
-    const [searchTerm, setSearchTerm] = React.useState('');
-
-    const handleChange = event => {
-        setSearchTerm(event.target.value);
-    };
-
-    return (
-        <div>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" onChange={handleChange}/>
-
-            <p>
-                Searching for <strong>{searchTerm}</strong>
-            </p>
-        </div>
-    );
-};
+export const Search = ({search, onSearch}) => (
+    <div>
+        <label htmlFor="search">Search: </label>
+        <input
+            id="search"
+            type="text"
+            value={search}
+            onChange={onSearch}
+        />
+    </div>
+);

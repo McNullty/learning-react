342d9de52ba50df07a287f4d44b69396bc93d50e
Mladen Čikara <mladen.cikara@gmail.com>
2020-06-01 10:47:18 +0200

    Fetching only on submit button

--- src/App.js
+++ src/App.js
@@ -103,7 +103,11 @@ const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
 
-    const [searchTerm, setSearchTerm] = useSemiPersistentState('search');
+    const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+    const [url, setUrl] = React.useState(
+        `${API_ENDPOINT}${searchTerm}`
+    );
 
     const [stories, dispatchStories] = React.useReducer(
         storiesReducer,
@@ -115,7 +119,7 @@ const App = () => {
 
         dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-        fetch(`${API_ENDPOINT}${searchTerm}`)
+        fetch(url)
             .then(response => response.json())
             .then(result => {
                 dispatchStories({
@@ -126,7 +130,7 @@ const App = () => {
             .catch(() =>
                 dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
             );
-    }, [searchTerm]);
+    }, [url]);
 
     React.useEffect(() => {
         handleFetchStories();
@@ -139,8 +143,12 @@ const App = () => {
         });
     };
 
-    const handleSearch = event => {
+    const handleSearchInput = event => {
         setSearchTerm(event.target.value);
+    };
+
+    const handleSearchSubmit = () => {
+        setUrl(`${API_ENDPOINT}${searchTerm}`)
     };
 
     return (
@@ -151,10 +159,14 @@ const App = () => {
                 id="search"
                 value={searchTerm}
                 isFocused
-                onInputChange={handleSearch}
+                onInputChange={handleSearchInput}
             >
                 <strong>Search:</strong>
             </InputWithLabel>
+
+            <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
+                Submit
+            </button>
 
             <hr/>
 

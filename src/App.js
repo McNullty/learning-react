fc947e6819f3b7c9692fa33d65393617dadbe0db
Mladen Čikara <mladen.cikara@gmail.com>
2020-06-01 10:02:38 +0200

    Calling only remote fetch when search term is changed

--- src/App.js
+++ src/App.js
@@ -99,6 +99,8 @@ const getAsyncStories = () =>
             2000)
     );
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
 const App = () => {
 
     const [searchTerm, setSearchTerm] = useSemiPersistentState('search');
@@ -109,19 +111,22 @@ const App = () => {
     );
 
     React.useEffect(() => {
+        if (!searchTerm) return;
+
         dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-        getAsyncStories()
+        fetch(`${API_ENDPOINT}${searchTerm}`)
+            .then(response => response.json())
             .then(result => {
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.data.stories,
+                    payload: result.hits,
                 });
             })
             .catch(() =>
                 dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
             );
-    }, []);
+    }, [searchTerm]);
 
     const handleRemoveStory = item => {
         dispatchStories({
@@ -133,10 +138,6 @@ const App = () => {
     const handleSearch = event => {
         setSearchTerm(event.target.value);
     };
-
-    const searchStories = stories.data.filter(story => story.title
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase()));
 
     return (
         <div>
@@ -157,7 +158,7 @@ const App = () => {
 
             {stories.isLoading ?
                 (<p>Loading...</p>) :
-                (<List list={searchStories} onRemoveStory={handleRemoveStory}/>)}
+                (<List list={stories.data} onRemoveStory={handleRemoveStory}/>)}
 
             <hr/>
             <div>Footer</div>

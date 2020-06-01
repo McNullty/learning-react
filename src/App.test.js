0949bf9d4c1b318e35b24d5cd510473ceeec1f5e
Mladen Čikara <mladen.cikara@gmail.com>
2020-06-01 22:34:41 +0200

    Test with mocked fetching from remote api

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer"
+import axios from 'axios'
 
-import {List, Item} from "./App";
+import App, { List, Item} from "./App";
+
+jest.mock('axios');
 
 describe('Item', () => {
   const item = {
@@ -63,5 +66,58 @@ describe('List', () => {
     const component = renderer.create(<List list={list} onRemoveItem={handleRemoveItem}/>);
 
     expect(component.root.findAllByType(Item).length).toEqual(2);
+  });
+});
+
+describe('App', () => {
+  it('succeeds fetching data with a list', async () => {
+    const list = [
+      {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      },
+      {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+      },
+    ];
+
+    const promise = Promise.resolve({
+      data: {
+        hits: list,
+      },
+    });
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    let component;
+
+    await renderer.act(async () => {
+      component = renderer.create(<App />);
+    });
+
+    expect(component.root.findByType(List).props.list).toEqual(list);
+  });
+
+  it('fails fetching data with a list', async () => {
+    const promise = Promise.reject();
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    let component;
+
+    await renderer.act(async () => {
+      component = renderer.create(<App />);
+    });
+
+    expect(component.root.findByType('p').props.children).toEqual('Something went wrong ...');
   });
 });
